7891c337fa9ac540cd5dfce13e59cebc860a53df
Rafael Jürkfitz <rafaeljurkfitz@hotmail.com>
2021-12-26 20:18:32 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+const menuBtn = document.querySelector('#hamburguer');
+const aside = document.querySelector('aside');
+const searchButton = document.querySelector('.search');
+const searchField = document.querySelector('#search');
+
+menuBtn.addEventListener('click', () => {
+	aside.classList.toggle('open');
+});
+
+searchButton.addEventListener('click', () => {
+	isOpen = aside.classList.value = 'open';
+
+    searchField.focus();
+	if (!isOpen) {
+		aside.classList.toggle('open');
+	}
+});
